19d972c760505fa49650c9fb1c76f9fe9543d411
Anastasiya Soloveva <102226072+Solonasty@users.noreply.github.com>
2023-02-28 15:50:51 +0300

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -2,36 +2,36 @@
 
 let form = document.getElementById("form");
 let submitBtn = document.getElementById("submit-btn");
-const clearBtn = document.getElementById("clear-btn");
-const output = document.getElementById("json");
+let clearBtn = document.getElementById("clear-btn");
+let output = document.getElementById("json");
 
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
   // Object for store form
-  const formData = {};
+  let formData = {};
 
   // Add all list data to formData
-  const dropdowns = form.querySelectorAll("select");
+  let dropdowns = form.querySelectorAll("select");
   dropdowns.forEach((dropdown) => {
     formData[dropdown.name] = dropdown.value;
   });
 
   // Add all input data to formData
-  const inputs = form.querySelectorAll("input[type='text']");
+  let inputs = form.querySelectorAll("input[type='text']");
   inputs.forEach((input) => {
     formData[input.name] = input.value;
   });
 
   // formData to string JSON
-  const jsonString = JSON.stringify(formData);
+  let jsonString = JSON.stringify(formData);
 
   // output JSON
   output.innerText = jsonString;
 
   // send GET-request
 
-  const xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open('GET', '#.php'); // replace '#.php' with link on server
   xhr.setRequestHeader('Content-Type', 'application/json');
 
