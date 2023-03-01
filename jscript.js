be8164911a4e7d4c2cb35f4f669d7cb436da1333
Anastasiya Soloveva <102226072+Solonasty@users.noreply.github.com>
2023-03-01 10:00:51 +0300

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -9,12 +9,12 @@ submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
   // Object for store form
-  let formData = {};
+  let formData = {}; // typeof == object
 
   // Add all list data to formData
   let dropdowns = form.querySelectorAll("select");
   dropdowns.forEach((dropdown) => {
-    formData[dropdown.name] = dropdown.value;
+    formData[dropdown.name] = dropdown.value; // select name list1 = option value 1
   });
 
   // Add all input data to formData
@@ -24,27 +24,28 @@ submitBtn.addEventListener("click", (event) => {
   });
 
   // formData to string JSON
-  let jsonString = JSON.stringify(formData);
+  let jsonString = JSON.stringify(formData); // typeof == string
 
   // output JSON
   output.innerText = jsonString;
 
-  // send GET-request
 
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', '#.php'); // replace '#.php' with link on server
-  xhr.setRequestHeader('Content-Type', 'application/json');
+// send GET-request
 
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      alert(xhr.responseText);
+  let xhr = new XMLHttpRequest(); //1. Создать XMLHttpRequest
+  xhr.open('GET', '#.php'); // 2. Инициализировать его xhr.open(method(GET/POST), URL) (replace '#.php' with folder on server)
+
+  xhr.send(); //3. Послать запрос
+
+  xhr.onload = function() { //4. Слушать события на xhr, чтобы получить ответ (load – происходит, когда получен какой-либо ответ)
+
+    if (xhr.status === 200) { //Код состояния HTTP (число): 200, 404, 403 и так далее, может быть 0 в случае, если ошибка не связана с HTTP.
+      alert(xhr.response);
     }
     else {
       alert('Произошла ошибка. Код ошибки: ' + xhr.status);
     }
   };
-
-  xhr.send();
 
 });
 
